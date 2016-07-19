5c1f6dbf20f32331d577a7ccfec8d20dfba75d78
Igor Kleshev <membship@ya.ru>
2016-07-19 14:17:23 +0700

    commit

--- www/wp-content/themes/son/archive.php
+++ www/wp-content/themes/son/archive.php
@@ -35,6 +35,7 @@
   </div><!--slider-->
   <div class="w_1200 main">
     <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 col-content">
+      <h1><?php single_cat_title(); ?></h1>
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>               
         <ul class="categor">
           <li><a href="<?php the_permalink();?>"><?php the_title(); ?></a></li>
--- www/wp-content/themes/son/js/common.js
+++ www/wp-content/themes/son/js/common.js
@@ -47,4 +47,60 @@ $(function() {
 
 	$('.last_art .rpwe-clearfix').attr('class', '').attr('class','col-lg-4 col-md-4 col-sm-6 col-xs-12 rpwe-clearfix');
 
-});
+	var wor = $('.two_words_about_us .two_words_about_us_like_h2').html();
+	if (wor !== undefined) {
+		var wor2 = wor.split(' ');
+		wor2[4] = '<b>'+wor2[4]+'</b>';
+		wor = '';
+		for(var i=0; i <= (wor2.length-1); i++) {			
+			wor = wor+' '+wor2[i];
+		}
+		$('.two_words_about_us .two_words_about_us_like_h2').html(wor);
+	}
+
+
+	var la = $('.last_art .like_h2').html();
+	if (la !== undefined) {
+		var la2 = la.split(' ');
+		la2[1] = '<b>'+la2[1]+'</b>';
+		la = '';
+		for(var i=0; i <= (la2.length-1); i++) {			
+			la = la+' '+la2[i];
+		}
+		$('.last_art .like_h2').html(la);
+	}
+
+	var con = $('.col-content h1').html();
+	if (con !== undefined) {
+		var con2 = con.split(' ');
+		con2[0] = '<b>'+con2[0]+'</b>';
+		con = '';
+		for(var i=0; i <= (con2.length-1); i++) {			
+			con = con+' '+con2[i];
+		}
+		$('.col-content h1').html(con);
+	}
+
+
+	/*var con = $('.col-content h1').html();
+	if (con !== undefined) {
+		var con2 = con.split(' ');
+		con2[0] = '<b>'+con2[0]+'</b>';
+		var con = $('.col-content h1').html(con2[0]+' '+con2[1]);
+	}*/
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+}); //ready
